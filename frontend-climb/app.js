5542291815d6562ee9d453ca07277fd8b7092d60
Andrew Thiessen <squaids449@gmail.com>
2019-10-25 11:10:29 -0600

    +added footer +added about page =styled about page =styled google map to night mode

--- frontend-climb/app.js
+++ frontend-climb/app.js
@@ -12,7 +12,13 @@ form.addEventListener("submit", event => {
     const distance = formData.get("distance")
     results.textContent = `Here are climbs within ${distance} miles of you:`
     if(distance <= 11){
-      map.setZoom(12)
+      map.setZoom(11)
+    }else if(distance <= 20){
+      map.setZoom(10)
+    }else if(distance <= 31){
+      map.setZoom(9)
+    }else if(distance >= 42){
+      map.setZoom(8.5)
     }
     deleteMarkers()
     
@@ -26,18 +32,111 @@ var map;
 function initMap() {
     map = new google.maps.Map(document.getElementById('map'), {
         center: {lat: 39.769021, lng: -104.974148},
-        zoom: 9
+        zoom: 9,
+        styles: [
+          {elementType: 'geometry', stylers: [{color: '#242f3e'}]},
+          {elementType: 'labels.text.stroke', stylers: [{color: '#242f3e'}]},
+          {elementType: 'labels.text.fill', stylers: [{color: '#746855'}]},
+          {
+            featureType: 'administrative.locality',
+            elementType: 'labels.text.fill',
+            stylers: [{color: '#d59563'}]
+          },
+          {
+            featureType: 'poi',
+            elementType: 'labels.text.fill',
+            stylers: [{color: '#d59563'}]
+          },
+          {
+            featureType: 'poi.park',
+            elementType: 'geometry',
+            stylers: [{color: '#263c3f'}]
+          },
+          {
+            featureType: 'poi.park',
+            elementType: 'labels.text.fill',
+            stylers: [{color: '#6b9a76'}]
+          },
+          {
+            featureType: 'road',
+            elementType: 'geometry',
+            stylers: [{color: '#38414e'}]
+          },
+          {
+            featureType: 'road',
+            elementType: 'geometry.stroke',
+            stylers: [{color: '#212a37'}]
+          },
+          {
+            featureType: 'road',
+            elementType: 'labels.text.fill',
+            stylers: [{color: '#9ca5b3'}]
+          },
+          {
+            featureType: 'road.highway',
+            elementType: 'geometry',
+            stylers: [{color: '#746855'}]
+          },
+          {
+            featureType: 'road.highway',
+            elementType: 'geometry.stroke',
+            stylers: [{color: '#1f2835'}]
+          },
+          {
+            featureType: 'road.highway',
+            elementType: 'labels.text.fill',
+            stylers: [{color: '#f3d19c'}]
+          },
+          {
+            featureType: 'transit',
+            elementType: 'geometry',
+            stylers: [{color: '#2f3948'}]
+          },
+          {
+            featureType: 'transit.station',
+            elementType: 'labels.text.fill',
+            stylers: [{color: '#d59563'}]
+          },
+          {
+            featureType: 'water',
+            elementType: 'geometry',
+            stylers: [{color: '#17263c'}]
+          },
+          {
+            featureType: 'water',
+            elementType: 'labels.text.fill',
+            stylers: [{color: '#515c6d'}]
+          },
+          {
+            featureType: 'water',
+            elementType: 'labels.text.stroke',
+            stylers: [{color: '#17263c'}]
+          }
+        ]
     });
 }
 
 function addMarkerWithTimeout(position, timeout, climb) {
   window.setTimeout(function() {
-    markers.push(new google.maps.Marker({
-      position: position,
-      map: map,
-      title: `${climb.name} | ${climb.type} ${climb.rating}`,
-      animation: google.maps.Animation.DROP
-    }));
+    var marker = new google.maps.Marker({
+        position: position,
+        map: map,
+        title: `${climb.name} | ${climb.type} ${climb.rating}`,
+        animation: google.maps.Animation.DROP
+    });
+    var infowindow = new google.maps.InfoWindow({
+        content: `<div class='info-window'><h3><a href='${climb.url}' target='_blank' class='info-title'>${climb.name}</a></h3>`+
+        `<p class='sub-info'>Type:${climb.type} - Rated:${climb.rating}</p>`+
+        `<img src="${climb.imgSmall}" class='info-image'>`+
+        `</div>`
+    });
+    marker.addListener('dblclick', function() {
+            infowindow.open(map, marker);
+      });
+    marker.addListener('click', function() {
+        infowindow.close();
+    });
+    markers.push(marker)
   }, timeout);
 }
 
